408bd9ab9063c217163a04bbe0ff82c0d728b9c5
Jhonny Córdova <cordova.jhonny1993@gmail.com>
2019-10-20 11:22:58 -0300

    url project

--- api/app/Services/NasaImagesService.php
+++ api/app/Services/NasaImagesService.php
@@ -8,13 +8,13 @@ class NasaImagesService {
     public function getNasaImageId(){
 
         $nasaIdsImages = [];
-        $nasaIdsImages[] = ['id' => 'GSFC_20171208_Archive_e000201', 'url_project' => 'https://ovacen.com/100-proyectos-ecologicos/'];
-        $nasaIdsImages[] = ['id' => '200910220009HQ', 'url_project' => 'https://ovacen.com/100-proyectos-ecologicos/'];
-        $nasaIdsImages[] = ['id' => 'GSFC_20171208_Archive_e001453', 'url_project' => 'https://ovacen.com/100-proyectos-ecologicos/'];
-        $nasaIdsImages[] = ['id' => 'GSFC_20171208_Archive_e000271', 'url_project' => 'https://ovacen.com/100-proyectos-ecologicos/'];
-        $nasaIdsImages[] = ['id' => 'ED04-0056-105', 'url_project' => 'https://ovacen.com/100-proyectos-ecologicos/'];
-        $nasaIdsImages[] = ['id' => 'GSFC_20171208_Archive_e000988', 'url_project' => 'https://ovacen.com/100-proyectos-ecologicos/'];
-        $nasaIdsImages[] = ['id' => 'PIA18054', 'url_project' => 'https://ovacen.com/100-proyectos-ecologicos/'];
+        $nasaIdsImages[] = ['id' => 'GSFC_20171208_Archive_e000201', 'url_project' => 'https://www.naturalcapitalpartners.com/solutions/solution/carbon-emissions'];
+        $nasaIdsImages[] = ['id' => '200910220009HQ', 'url_project' => 'https://www.naturalcapitalpartners.com/solutions/solution/carbon-emissions'];
+        $nasaIdsImages[] = ['id' => 'GSFC_20171208_Archive_e001453', 'url_project' => 'https://www.naturalcapitalpartners.com/solutions/solution/carbon-emissions'];
+        $nasaIdsImages[] = ['id' => 'GSFC_20171208_Archive_e000271', 'url_project' => 'https://www.naturalcapitalpartners.com/solutions/solution/carbon-emissions'];
+        $nasaIdsImages[] = ['id' => 'ED04-0056-105', 'url_project' => 'https://www.naturalcapitalpartners.com/solutions/solution/carbon-emissions'];
+        $nasaIdsImages[] = ['id' => 'GSFC_20171208_Archive_e000988', 'url_project' => 'https://www.naturalcapitalpartners.com/solutions/solution/carbon-emissions'];
+        $nasaIdsImages[] = ['id' => 'PIA18054', 'url_project' => 'https://www.naturalcapitalpartners.com/solutions/solution/carbon-emissions'];
 
         $index = array_rand($nasaIdsImages);
         $image = $nasaIdsImages[$index];
