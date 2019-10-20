9e0a388a056831ea81f739948a0de32c00704f42
Leonel Rojas <leonelrojasperez@gmail.com>
2019-10-19 21:50:33 -0300

    alerts

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import image_slicer
-image_slicer.slice('assets/images/logo-space-apps-cl.png', 4)
+
+import urllib2
+
+filedata = urllib2.urlopen('http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg')
+datatowrite = filedata.read()
+ 
+with open('assets/images/test.jpg', 'wb') as f:
+    f.write(datatowrite)
+
+#image_slicer.slice('assets/images/logo-space-apps-cl.png', 4)
