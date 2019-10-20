c64b2536ae8e2459cb6247d33d5a2c955001afdd
Jhonny Córdova <cordova.jhonny1993@gmail.com>
2019-10-19 21:51:03 -0300

    Endpoint para obtener imagenes

---
+++ api/app/Http/Controllers/ImageController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+
+class ImageController extends Controller
+{
+    public function getImage()
+    {
+        $urlAPI = 'https://images-api.nasa.gov/search?q=fires';
+        $client = new Client();
+        $response = $client->request('GET', $urlAPI);
+        $data = json_decode((string)$response->getBody(), true);
+        
+        $info = [];
+        if (count($data['collection']['items'])) {
+            $images = $data['collection']['items'];
+            $imageIndex = array_rand($images);
+
+            $image = $images[$imageIndex];
+
+            $urlImage = $image['links'][0]['href'];
+            $timestamp = time();
+            $fileName = './images/' . $timestamp . '.png';
+
+            $process = new Process("python3 ./../../main.py \"{$urlImage}\" \"{$fileName}\" ");
+            $process->run();
+
+            // executes after the command finishes
+            if (!$process->isSuccessful()) {
+                throw new ProcessFailedException($process);
+            }
+
+            $info = [
+                'images' => [
+                    url("/images/{$timestamp}_02_01.png"),
+                    url("/images/{$timestamp}_01_01.png"),
+                    url("/images/{$timestamp}_01_02.png"),
+                    url("/images/{$timestamp}_02_02.png"),
+                ],
+                'info' => [
+                    'title' => $image['data'][0]['title'],
+                    'description' => $image['data'][0]['description'],
+                    'created_at' => $image['data'][0]['date_created'],
+                ]
+            ];
+
+        }
+
+        return response()->json(['data' => $info], 200);
+    }
+}
--- main.py
+++ main.py
@@ -1,2 +1,19 @@
+#!/usr/bin/python
+import sys
 import image_slicer
-image_slicer.slice('assets/images/logo-space-apps-cl.png', 4)
+
+import urllib3
+http = urllib3.PoolManager()
+r = http.request('GET', sys.argv[1], preload_content=False)
+with open(sys.argv[2], 'wb') as out:
+   while True:
+       data = r.read(100)
+       if not data:
+           break
+       out.write(data)
+r.release_conn()
+
+image_slicer.slice(sys.argv[2], 4)
+
+# tiles = image_slicer.slice('../assets/images/logo-space-apps-cl.png', 4, save=False)
+# image_slicer.save_tiles(tiles, directory='~/cake_slices')
